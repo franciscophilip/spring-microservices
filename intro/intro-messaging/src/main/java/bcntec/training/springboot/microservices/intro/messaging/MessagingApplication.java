5b0d78fbd59467bb16a1068b3e5e36a51092c517
fphilip <fphilip@houseware.es>
2018-12-04 13:22:43 +0100

    messaging

--- intro/intro-messaging/src/main/java/bcntec/training/springboot/microservices/intro/messaging/MessagingApplication.java
+++ intro/intro-messaging/src/main/java/bcntec/training/springboot/microservices/intro/messaging/MessagingApplication.java
@@ -26,6 +26,7 @@ public class MessagingApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+
         sender.send("Hello Messaging..!!!");
     }
 
--- intro/intro-messaging/src/main/java/bcntec/training/springboot/microservices/intro/messaging/Receiver.java
+++ intro/intro-messaging/src/main/java/bcntec/training/springboot/microservices/intro/messaging/Receiver.java
@@ -1,8 +1,10 @@
 package bcntec.training.springboot.microservices.intro.messaging;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
+@ConditionalOnProperty(value = "demo.mode",havingValue = "listen")
 @Component
 class Receiver {
     @RabbitListener(queues = "TestQ")
